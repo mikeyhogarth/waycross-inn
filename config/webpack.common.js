ca1b6ee9c19e7ee14b29bf8fcbd97200441418bc
Mike Hogarth <mikehogarth20@hotmail.com>
2016-09-07 18:41:23 +0100

    Change loader definitions

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -17,36 +17,43 @@ module.exports = {
   module: {
     loaders: [
       {
+        // Typescript
         test: /\.ts$/,
         loaders: ['ts', 'angular2-template-loader']
       },
       {
+        // HTML (TODO: Deprecate)
         test: /\.html$/,
         loader: 'html'
       },
       {
+        // Templates (pug based)
         test: /\.pug$/,
         exclude: /node_modules/,
-        loaders: ['pug-loader'] // sass-loader not scss-loader
+        loaders: ['pug-loader']
       },
       {
+        // component based scss - load raw, fall back to angular pipeline
         test: /\.scss$/,
-        exclude: /node_modules/,
-        loaders: ['raw-loader', 'sass-loader'] // sass-loader not scss-loader
+        include: helpers.root('src', 'components'),
+        loaders: ['raw-loader', 'sass-loader']
+      },      
+      {
+        // public scss - squirt in using the style loader
+        test: /\.scss$/,
+        exclude: ['node_modules', helpers.root('src', 'components')],
+        loaders: ['style-loader', 'raw-loader', 'sass-loader'] // sass-loader not scss-loader
       },
       {
+        // Images
         test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
         loader: 'file?name=assets/[name].[hash].[ext]'
       },
       {
+        // public css files - squirt in using the style loader
         test: /\.css$/,
-        exclude: helpers.root('src', 'app'),
+        exclude: helpers.root('src', 'components'),
         loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
-      },
-      {
-        test: /\.css$/,
-        include: helpers.root('src', 'app'),
-        loader: 'raw'
       }
     ]
   },
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './components';
-import 'style!../public/stylesheets/styles.scss';
+import '../public/stylesheets/styles.scss';
 
 if (process.env.ENV === 'production') {
   enableProdMode();
